6a85022d884bc302438ae8df27d73b8c7ddf58a0
Sindri Már Kaldal Sigurjónsson <sindrikaldal@n157-p245.eduroam.kth.se>
2017-10-11 18:39:10 +0200

    core now starts two seperate streams for both home and away team

--- streamer/core.py
+++ streamer/core.py
@@ -5,13 +5,22 @@ from requests.packages.urllib3.exceptions import ProtocolError
 from streamer.StreamListener import MyStreamListener
 from configparser import ConfigParser
 from streamer.rabbitmq import RabbitMQ
+from threading import Thread
 
+
+def twitter_filter(stream, tweets):
+    while True:
+        try:
+            stream.filter(track=tweets)
+        except IncompleteRead:
+            print('Incomplete Read')
+        except ProtocolError:
+            print('Protocol Error')
 
 if __name__ == '__main__':
-    param = input('Teams: ').split()
-    if len(param) != 2:
-        print('Two teams should be playing.')
-        sys.exit(0)
+    home_team = input('Home Team: ').split()
+    away_team = input('Away Team: ').split()
+
     config = ConfigParser()
     config.read('config.ini')
     # twitter_topics = input('Please enter topics to stream separated with space: ').split()
@@ -20,20 +29,25 @@ if __name__ == '__main__':
     auth.set_access_token(config.get('twitter', 'access_token'), config.get('twitter', 'access_token_secret'))
     api = tweepy.API(auth)
 
-    my_stream_listener = MyStreamListener()
+    home_team_listener = MyStreamListener()
+    away_team_listener = MyStreamListener()
     rabbitmq = RabbitMQ(
         config.get('pika', 'rabbitmq_user'),
         config.get('pika', 'rabbitmq_pw'),
         config.get('pika', 'rabbitmq_host')
     )
-    my_stream_listener.set_queue(rabbitmq, ''.join(param))
+
+    home_team_listener.set_queue(rabbitmq, ' '.join(home_team))
+    away_team_listener.set_queue(rabbitmq, ' '.join(away_team))
     # twitter_topics = ['#' + param[0][:3] + param[1][:3]]
-    twitter_topics = ['#WorldMentalHealthDay']
-    my_stream = tweepy.Stream(auth=api.auth, listener=my_stream_listener)
-    while True:
-        try:
-            my_stream.filter(track=twitter_topics)
-        except IncompleteRead:
-            print('Incomplete Read')
-        except ProtocolError:
-            print('Protocol Error')
+    home_team_tweets = [' '.join(home_team)]
+    away_team_tweets = [' '.join(away_team)]
+    home_team_stream = tweepy.Stream(auth=api.auth, listener=home_team_listener)
+    away_team_stream = tweepy.Stream(auth=api.auth, listener=away_team_listener)
+
+    home_team_thread = Thread(target=twitter_filter, args=[home_team_stream, home_team_tweets])
+    home_team_thread.start()
+
+    away_team_thread = Thread(target=twitter_filter, args=[away_team_stream, away_team_tweets])
+    away_team_thread.start()
+
