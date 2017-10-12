caabad77f12af0e6fbe0d5b979bc336bfbf43d68
Sindri Már Kaldal Sigurjónsson <sindrikaldal@n134-p229.eduroam.kth.se>
2017-10-12 17:49:43 +0200

    minor fix

--- streamer/analyser.py
+++ streamer/analyser.py
@@ -16,8 +16,6 @@ class Analyser:
         self.home_team = Team(home_team)
         self.away_team = Team(away_team)
         self.config = ConfigParser()
-        self.away_team_collection = None
-        self.home_team_collection = None
         self.rabbitmq = None
         self.premier_league = None
         self.setup()
@@ -37,10 +35,6 @@ class Analyser:
         mongo_client = MongoClient('mongodb://%s:%s@' % (username, password) + self.config.get('db', 'db_url'))
         db_connection = mongo_client[self.config.get('db', 'db_name')]
 
-        # Get the collections for both teams
-        self.home_team_collection = db_connection[self.home_team.name]
-        self.away_team_collection = db_connection[self.away_team.name]
-
         print('Database connection to {}.{} established'.format(self.config.get('db', 'db_name'), 'test'))
 
     def init_rabbitmq(self):
@@ -54,8 +48,8 @@ class Analyser:
         )
 
         # Initialize a separate rabbitMQ thread for each team to extract information for the tweets
-        self.rabbitmq.init_consumer_thread(self.home_team.name, self.tweet_callback)
-        self.rabbitmq.init_consumer_thread(self.away_team.name, self.tweet_callback)
+        self.rabbitmq.init_consumer_thread(self.home_team.name, self.home_team_tweet_callback)
+        self.rabbitmq.init_consumer_thread(self.away_team.name, self.away_team_tweet_callback)
 
         print('Rabbit mq connection to {} : {} - {} established'.format(self.config.get('pika', 'rabbitmq_host'),
                                                                         self.home_team.name, self.away_team.name))
@@ -63,7 +57,13 @@ class Analyser:
     def init_premier_league(self):
         self.premier_league = PremierLeague()
 
-    def tweet_callback(self, ch, method, properties, body):
+    def home_team_tweet_callback(self, ch, method, properties, body):
+        self.tweet_analyser(self.home_team, body)
+
+    def away_team_tweet_callback(self, ch, method, properties, body):
+        self.tweet_analyser(self.away_team, body)
+
+    def tweet_analyser(self, team, body):
 
         # Extract the json from the tweet
         json_body = json.loads(body.decode('utf-8'))
@@ -74,10 +74,7 @@ class Analyser:
         polarity = text_blob.sentiment.polarity
 
         json_body['polarity'] = polarity
-        if abs(polarity) > 0.2:
-            print('{} | Polarity: {}'.format(text, text_blob.sentiment.polarity))
-            print('Tags: {}'.format(text_blob.tags))
-            print('Nouns: {}'.format(text_blob.noun_phrases))
+
         print(json_body)
         # object_id = self.db_collection.insert_one(json_body).inserted_id
         # print('{}: {}'.format(object_id, text))
